475583e2a7dd14793c99f0bdabbb645468f70d9e
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-08-20 15:07:34 -0300

    feat(web): added autentication configure to web and coupon api

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -2,12 +2,14 @@
 using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers;
 
 [Route("api/coupon")]
 [ApiController]
+[Authorize]
 public class CouponAPIController : ControllerBase
 {
     private readonly AppDbContext _db;
@@ -73,6 +75,7 @@ public class CouponAPIController : ControllerBase
     }
 
     [HttpPost]
+    [Authorize(Roles = "ADMIN")]
     public ResponseDto Post([FromBody] CouponDto couponDto)
     {
         try
@@ -92,6 +95,7 @@ public class CouponAPIController : ControllerBase
     }
 
     [HttpPut]
+    [Authorize(Roles = "ADMIN")]
     public ResponseDto Put([FromBody] CouponDto couponDto)
     {
         try
@@ -112,6 +116,7 @@ public class CouponAPIController : ControllerBase
 
     [HttpDelete]
     [Route("{id:int}")]
+    [Authorize(Roles = "ADMIN")]
     public ResponseDto Delete(int id)
     {
         try
