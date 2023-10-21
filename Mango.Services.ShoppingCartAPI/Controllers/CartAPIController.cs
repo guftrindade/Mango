dc65b45250409605bfd597b01791b2214e5efe78
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-10-21 20:24:09 -0300

    feat(cart): productApi and couponApi integrated to cartApi

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -2,6 +2,7 @@
 using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,22 +15,92 @@ public class CartAPIController : ControllerBase
     private readonly AppDbContext _db;
     private readonly IMapper _mapper;
     private ResponseDto _response;
+    private IProductService _productService;
+    private ICouponService _couponService;
 
-    public CartAPIController(AppDbContext db, IMapper mapper)
+    public CartAPIController(AppDbContext db, IMapper mapper, IProductService productService, ICouponService couponService)
     {
         _db = db;
         _response = new ResponseDto();
         _mapper = mapper;
+        _productService = productService;
+        _couponService = couponService;
+    }
+
+    [HttpGet("GetCart/{userId}")]
+    public async Task<ResponseDto> GetCart(string userId)
+    {
+        try
+        {
+            CartDto cart = new()
+            {
+                CartHeader = _mapper.Map<CartHeaderDto>(_db.CartHeaders.First(u => u.UserId == userId))
+            };
+
+            cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails.Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId));
+
+            var productDtos = await _productService.GetProducts();
+
+            foreach(var item in cart.CartDetails) 
+            {
+                item.Product = productDtos.FirstOrDefault(u => u.ProductId == item.ProductId);
+                cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+            }
+
+            if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
+            {
+                CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
+
+                if(coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
+                {
+                    cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+                    cart.CartHeader.Discount = coupon.DiscountAmount;
+                }
+            }
+
+            _response.Result = cart;
+
+        }
+        catch (Exception ex)
+        {
+            _response.Message = ex.Message.ToString();
+            _response.IsSuccess = false;
+        }
+
+        return _response;
+    }
+
+    [HttpPost("ApplyCoupon")]
+    public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+    {
+        try
+        {
+            var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+            cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+
+            _db.CartHeaders.Update(cartFromDb);
+            await _db.SaveChangesAsync();
+
+            _response.Result = true;
+
+        }
+        catch (Exception ex)
+        {
+            _response.Message = ex.Message.ToString();
+            _response.IsSuccess = false;
+        }
+
+        return _response;
     }
 
     [HttpPost("CartUpsert")]
-    public async Task<ResponseDto> CartUpsert (CartDto cartDto)
+    public async Task<ResponseDto> CartUpsert(CartDto cartDto)
     {
         try
         {
             var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == cartDto.CartHeader.UserId);
 
-            if(cartHeaderFromDb == null)
+            if (cartHeaderFromDb == null)
             {
                 CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
                 _db.CartHeaders.Add(cartHeader);
@@ -46,7 +117,7 @@ public class CartAPIController : ControllerBase
                     u => u.ProductId == cartDto.CartDetails.First().ProductId &&
                     u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
 
-                if(cartDetailsFromDb == null)
+                if (cartDetailsFromDb == null)
                 {
                     cartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.CartHeaderId;
                     var cartDetails = _mapper.Map<CartDetails>(cartDto.CartDetails.First());
@@ -66,6 +137,36 @@ public class CartAPIController : ControllerBase
             }
 
             _response.Result = cartDto;
+
+        }
+        catch (Exception ex)
+        {
+            _response.Message = ex.Message.ToString();
+            _response.IsSuccess = false;
+        }
+
+        return _response;
+    }
+
+    [HttpPost("RemoveCart")]
+    public async Task<ResponseDto> RemoveCart([FromBody] int cartDetailsId)
+    {
+        try
+        {
+            CartDetails cartDetails = _db.CartDetails.First(u => u.CartDetailsId == cartDetailsId);
+
+            int totalCountOfCartItem = _db.CartDetails.Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+
+            _db.CartDetails.Remove(cartDetails);
+
+            if (totalCountOfCartItem == 1)
+            {
+                var cartHeaderToRemove = await _db.CartHeaders.FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+                _db.CartHeaders.Remove(cartHeaderToRemove);
+            }
+
+            await _db.SaveChangesAsync();
+            _response.Result = true;
 
         }
         catch (Exception ex)
--- Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -3,9 +3,12 @@
 public class CartDetailsDto
 {
     public int CartDetailsId { get; set; }
+
     public int CartHeaderId { get; set; }
-    //public CartHeader? CartHeader { get; set; }
+    public CartHeader? CartHeader { get; set; }
+
     public int ProductId { get; set; }
-    //public ProductDto? Product { get; set; }
+    public ProductDto? Product { get; set; }
+
     public int Count { get; set; }
 }
