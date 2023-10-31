0374abba24af4ffe10512eb004b928b431eb87a9
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-10-30 23:47:49 -0300

    feat(servicebus): message bus implementation

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus;
+
+public class MessageBus : IMessageBus
+{
+    public Task PublishMessage(object message, string topic_queue_Name)
+    {
+        throw new NotImplementedException();
+    }
+}
