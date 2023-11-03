b4f2c91681305df8432060d0ef3f2f211ee33fc9
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-11-03 10:08:39 -0300

    feat(email): setup email dto

---
+++ Mango.Service.EmailAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Mango.Service.EmailAPI.Models.Dto;
+
+public class CartDto
+{
+    public CartHeaderDto CartHeader { get; set; }
+    public IEnumerable<CartDetailsDto> CartDetails { get; set; }
+}
