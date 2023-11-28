03e1eee0a9b269d1c9c09f79c87427b8ecdfa9b2
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-11-28 08:25:46 -0300

    feat(ordermanagement): order list UI

---
+++ Mango.Web/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers;
+
+public class OrderController : Controller
+{
+    [Authorize]
+    public IActionResult OrderIndex()
+    {
+        return View();
+    }
+}
