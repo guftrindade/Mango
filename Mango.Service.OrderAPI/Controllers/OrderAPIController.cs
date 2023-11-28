ac7ae02a1911d1776f09da11fa897aa268f01516
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-11-28 08:13:49 -0300

    feat(ordermanagement): update order status endpoint

--- Mango.Service.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango.Service.OrderAPI/Controllers/OrderAPIController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Stripe.Checkout;
 using Stripe;
 using Mango.MessageBus;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Service.OrderAPI.Controllers;
 
@@ -31,6 +32,52 @@ public class OrderAPIController : ControllerBase
         _mapper = mapper;
         _messageBus = messageBus;
         _configuration = configuration;
+    }
+
+    [Authorize]
+    [HttpPost("GetOrders")]
+    public ResponseDto? Get(string? userId = "")
+    {
+        try
+        {
+            IEnumerable<OrderHeader> objList;
+
+            if (User.IsInRole(SD.RoleAdmin))
+            {
+                objList = _db.OrderHeaders.Include(u => u.OrderDetails).OrderByDescending(u => u.OrderHeaderId).ToList();
+            }
+            else
+            {
+                objList = _db.OrderHeaders.Include(u => u.OrderDetails).Where(u => u.UserId == userId).OrderByDescending(u => u.OrderHeaderId);
+            }
+
+            _response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(objList);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+
+        return _response;
+    }
+
+    [Authorize]
+    [HttpPost("GetOrder/{id:int}")]
+    public ResponseDto? Get(int id)
+    {
+        try
+        {
+            OrderHeader orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails).First(u => u.OrderHeaderId == id);
+            _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+
+        return _response;
     }
 
     [Authorize]
@@ -162,6 +209,40 @@ public class OrderAPIController : ControllerBase
         {
             _response.IsSuccess = false;
             _response.Message = ex.Message;
+        }
+
+        return _response;
+    }
+
+    [Authorize]
+    [HttpPost("UpdateOrderStatus/{orderId:int}")]
+    public async Task<ResponseDto> UpdateOrderStatus(int orderId, [FromBody] string newStatus)
+    {
+        try
+        {
+            OrderHeader orderHeader = _db.OrderHeaders.First(u => u.OrderHeaderId == orderId);
+
+            if (orderHeader != null)
+            {
+                if (newStatus == SD.Status_Cancelled)
+                {
+                    var options = new RefundCreateOptions
+                    {
+                        Reason = RefundReasons.RequestedByCustomer,
+                        PaymentIntent = orderHeader.PaymentIntentId
+                    };
+
+                    var service = new RefundService();
+                    Refund refund = service.Create(options);
+                }
+
+                orderHeader.Status = newStatus;
+                _db.SaveChanges();
+            }
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
         }
 
         return _response;
