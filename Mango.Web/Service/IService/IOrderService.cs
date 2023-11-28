510fcae60fb161bbf90ab9c5c049a9ac0fcd86a8
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-11-28 08:15:46 -0300

    feat(ordermanagement): endpoints to order service web

--- Mango.Web/Service/IService/IOrderService.cs
+++ Mango.Web/Service/IService/IOrderService.cs
@@ -7,4 +7,7 @@ public interface IOrderService
     Task<ResponseDto?> CreateOrder(CartDto cartDto);
     Task<ResponseDto?> CreateStripeSession(StripeRequestDto stripeRequestDto);
     Task<ResponseDto?> ValidateStripeSession(int orderHeaderId);
+    Task<ResponseDto?> GetAllOrder(string? userId);
+    Task<ResponseDto?> GetOrder(int orderId);
+    Task<ResponseDto?> UpdateOrderStatus(int orderId, string newStatus);
 }
