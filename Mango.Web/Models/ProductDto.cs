c45f048879a90e6d0b85c11afc3c24059249d99f
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-11-29 21:00:44 -0300

    feat(images): modify product model and dtos

--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -9,8 +9,10 @@ public class ProductDto
     public double Price { get; set; }
     public string Description { get; set; }
     public string CategoryName { get; set; }
-    public string ImageUrl { get; set; }
+	public string? ImageUrl { get; set; }
+	public string? ImageLocalPath { get; set; }
 
-    [Range(1,100)]
+	[Range(1,100)]
     public int Count { get; set; } = 1;
+    public IFormFile? Image {  get; set; }
 }
