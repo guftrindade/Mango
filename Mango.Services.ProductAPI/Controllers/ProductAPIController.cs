1452b0173c47175a9be76ee433dc3d5332f70eae
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-11-30 23:20:21 -0300

    feat(images): update and delete product with images

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -102,16 +102,46 @@ public class ProductAPIController : ControllerBase
 
     [HttpPut]
     [Authorize(Roles = "ADMIN")]
-    public ResponseDto Put([FromBody] ProductDto productDto)
+    public ResponseDto Put([FromForm] ProductDto productDto)
     {
         try
         {
-            Product obj = _mapper.Map<Product>(productDto);
-            _db.Products.Update(obj);
-            _db.SaveChanges();
+			Product product = _mapper.Map<Product>(productDto);
 
-            _response.Result = _mapper.Map<ProductDto>(obj);
-        }
+			if (productDto.Image != null)
+			{
+				if (!string.IsNullOrEmpty(product.ImageLocalPath))
+				{
+					var oldFilePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), product.ImageLocalPath);
+
+					FileInfo file = new(oldFilePathDirectory);
+
+					if (file.Exists)
+					{
+						file.Delete();
+					}
+				}
+
+				string fileName = product.ProductId + Path.GetExtension(productDto.Image.FileName);
+				string filePath = @"wwwroot\ProductImages\" + fileName;
+				var filePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+
+				using (var fileStream = new FileStream(filePathDirectory, FileMode.Create))
+				{
+					productDto.Image.CopyTo(fileStream);
+				}
+
+				var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+				product.ImageUrl = baseUrl + "/ProductImages/" + fileName;
+				product.ImageLocalPath = filePath;
+			}
+
+
+			_db.Products.Update(product);
+			_db.SaveChanges();
+
+			_response.Result = _mapper.Map<ProductDto>(product);
+		}
         catch (Exception ex)
         {
             _response.IsSuccess = false;
@@ -128,7 +158,20 @@ public class ProductAPIController : ControllerBase
         try
         {
             Product obj = _db.Products.First(x => x.ProductId == id);
-            _db.Products.Remove(obj);
+
+			if (!string.IsNullOrEmpty(obj.ImageLocalPath))
+			{
+				var oldFilePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), obj.ImageLocalPath);
+
+				FileInfo file = new(oldFilePathDirectory);
+
+				if (file.Exists)
+				{
+					file.Delete();
+				}
+			}
+
+			_db.Products.Remove(obj);
             _db.SaveChanges();
         }
         catch (Exception ex)
